5b729029c9bba6842b1cc663de691246f3aa0378
xuantung <tungdx@smartosc.com>
2019-09-11 10:06:26 +0700

    Fix ApiAbstract require paths

--- Controller/Contract/ApiAbstract.php
+++ Controller/Contract/ApiAbstract.php
@@ -1,7 +1,7 @@
 <?php
 if (!interface_exists("Magento\Framework\App\CsrfAwareActionInterface")) {
-    require($_SERVER['DOCUMENT_ROOT'] . '/vendor/connectpos/module-xretail/Controller/Contract/ApiAbstract22.php');
+    require(__DIR__ . '/ApiAbstract22.php');
 } else {
-    require ($_SERVER['DOCUMENT_ROOT'] . '/vendor/connectpos/module-xretail/Controller/Contract/ApiAbstract23.php');
+    require (__DIR__ . '/ApiAbstract23.php');
 }
 
