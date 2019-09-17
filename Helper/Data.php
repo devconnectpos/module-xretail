62a6efda2c13ff37949541f3e4814415c64ac1ae
xuantung <tungdx@smartosc.com>
2019-09-17 18:17:16 +0700

    Update version 19.09.17

--- Helper/Data.php
+++ Helper/Data.php
@@ -60,7 +60,7 @@ class Data extends AbstractHelper
     /**
      * @var string
      */
-    public static $API_VERSION = '19.09.06';
+    public static $API_VERSION = '19.09.17';
 
     /**
      * Data constructor.
