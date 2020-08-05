67ab5416ff83a4e5adf9aa92bceef2d57ce94494
xuantung <tungdx@smartosc.com>
2020-08-05 13:52:37 +0700

    Update version 20.08.05

--- Helper/Data.php
+++ Helper/Data.php
@@ -62,7 +62,7 @@ class Data extends AbstractHelper
     /**
      * @var string
      */
-    public static $API_VERSION = '20.07.30';
+    public static $API_VERSION = '20.08.05';
 	/**
 	 * @var OrderRepositoryInterface
 	 */
