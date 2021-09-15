e0a03914b118a199a7639ac558dc2bade58877ac
devconnectpos <51113258+devconnectpos@users.noreply.github.com>
2021-09-15 11:36:06 +0700

    Release 21.09.15 (#83)
    Co-authored-by: Khue Quang Nguyen <khuenq@connectpos.com>

--- Helper/Data.php
+++ Helper/Data.php
@@ -66,7 +66,7 @@ class Data extends AbstractHelper
     /**
      * @var string
      */
-    public static $API_VERSION = '21.09.13';
+    public static $API_VERSION = '21.09.15';
 
 	/**
 	 * @var OrderRepositoryInterface
