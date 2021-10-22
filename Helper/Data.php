737692a0918e1581687ae8f261482e53b5d1dda6
Khue Quang Nguyen <khuenq@connectpos.com>
2021-10-22 14:47:39 +0700

    Release 21.10.22

--- Helper/Data.php
+++ Helper/Data.php
@@ -65,7 +65,7 @@ class Data extends AbstractHelper
     /**
      * @var string
      */
-    public static $API_VERSION = '21.10.21';
+    public static $API_VERSION = '21.10.22';
 
 	/**
 	 * @var CollectionFactoryInterface
