31b1a5b42fd4c7a144acfbacd4574d828d6b9661
ConnectPOS <51113258+devconnectpos@users.noreply.github.com>
2022-01-12 14:21:41 +0700

    Release 22.01.12 (#112)
    Co-authored-by: Khue Quang Nguyen <khuenq@connectpos.com>

--- Helper/Data.php
+++ Helper/Data.php
@@ -65,7 +65,7 @@ class Data extends AbstractHelper
     /**
      * @var string
      */
-    public static $API_VERSION = '22.01.06';
+    public static $API_VERSION = '22.01.12';
 
 	/**
 	 * @var CollectionFactoryInterface
