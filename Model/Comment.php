7e995c55cf915d672c7d7233bcaf80103706b86b
Khue Quang Nguyen <khuenq@connectpos.com>
2022-03-29 10:41:37 +0700

    Release 22.03.29

--- Model/Comment.php
+++ Model/Comment.php
@@ -4,6 +4,7 @@
  * Date: 28/03/2018
  * Time: 10:37
  */
+
 namespace SM\XRetail\Model;
 
 use Magento\Config\Model\Config\CommentInterface;
@@ -12,15 +13,18 @@ use SM\XRetail\Helper\Data;
 class Comment implements CommentInterface
 {
     private $helper;
+
     public function __construct(
         Data $helper
     ) {
-        $this->helper          = $helper;
+        $this->helper = $helper;
     }
 
     public function getCommentText($elementValue)
     {
         $text = $this->helper->getCurrentVersion();
-        return "API Version: ".$text;
+        $notice = "We are developing a new version of ConnectPOS API connector modules. The legacy modules will soon be deprecated and we will notify you about the changes. Stay tuned!";
+
+        return "API Version: {$text}<br/><i>{$notice}</i>";
     }
 }
