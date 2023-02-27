dd4ecd3c2a4832e90353e21f5ccb45cac39b922e
ConnectPOS <51113258+devconnectpos@users.noreply.github.com>
2023-02-27 10:58:08 +0700

    Release 23.02.27 (#154)
    Co-authored-by: thaitt1 <thaitt1@smartosc.com>

--- Helper/Data.php
+++ Helper/Data.php
@@ -65,7 +65,7 @@ class Data extends AbstractHelper
     /**
      * @var string
      */
-    public static $API_VERSION = '23.02.25';
+    public static $API_VERSION = '23.02.27';
 
 	/**
 	 * @var CollectionFactoryInterface
